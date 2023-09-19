1284ee49bfdce77c53e2d0b48d58c6c79dabf85c
Neil Murphy <barrymun@tcd.ie>
2023-09-19 21:29:56 +0100

    working thumb on track for seek-bar element

--- src/components/seek-bar.ts
+++ src/components/seek-bar.ts
@@ -8,11 +8,15 @@ const { div } = van.tags;
 export const SeekBar = () => {
   const elapsedTime = van.state<string>("0:00");
   const duration = van.state<string>("0:00");
+  const left = van.state<number>(0);
 
   setInterval(() => {
     const musicVisualizer = App.getMusicVisualizer();
-    elapsedTime.val = secondsToMinSec(musicVisualizer.getElapsedTime());
-    duration.val = secondsToMinSec(musicVisualizer.getDuration());
+    const a = musicVisualizer.getElapsedTime();
+    const b = musicVisualizer.getDuration();
+    elapsedTime.val = secondsToMinSec(a);
+    duration.val = secondsToMinSec(b);
+    left.val = (a / b) * 100;
   }, 100);
 
   return div(
@@ -25,9 +29,15 @@ export const SeekBar = () => {
       },
       elapsedTime,
     ),
-    div({
-      class: "seek-bar-line",
-    }),
+    div(
+      {
+        class: "seek-bar-track",
+      },
+      div({
+        class: "seek-bar-thumb",
+        style: () => `left: ${left.val}%;`,
+      }),
+    ),
     div(
       {
         class: "total-time",
