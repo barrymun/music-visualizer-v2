bba3c7035b64933135b394329a41273745635e17
Neil Murphy <barrymun@tcd.ie>
2023-09-20 14:59:03 +0100

    working slider

--- src/components/seek-bar.ts
+++ src/components/seek-bar.ts
@@ -1,9 +1,12 @@
 import van from "vanjs-core";
+import { isNaN, isFinite } from "lodash";
 
 import { App } from "lib/app";
 import { secondsToMinSec } from "utils/helpers";
 
-const { div } = van.tags;
+const { div, input } = van.tags;
+
+let isDragging: boolean = false;
 
 export const SeekBar = () => {
   const elapsedTime = van.state<string>("0:00");
@@ -11,13 +14,36 @@ export const SeekBar = () => {
   const left = van.state<number>(0);
 
   setInterval(() => {
-    const musicVisualizer = App.getMusicVisualizer();
-    const a = musicVisualizer.getPlaybackTime();
-    const b = musicVisualizer.getDuration();
-    elapsedTime.val = secondsToMinSec(a);
-    duration.val = secondsToMinSec(b);
-    left.val = (a / b) * 100;
+    if (isDragging) return;
+    const mv = App.getMusicVisualizer();
+    const d = mv.getDuration();
+    const pt = mv.getPlaybackTime();
+    duration.val = secondsToMinSec(d);
+    elapsedTime.val = secondsToMinSec(pt);
+    const leftVal = (pt / d) * 100;
+    left.val = isNaN(leftVal) || !isFinite(leftVal) ? 0 : leftVal;
   }, 100);
+
+  const handleMouseDown = (_event: Event) => {
+    isDragging = true;
+  };
+
+  const handleMouseMove = (_event: Event) => {
+    if (!isDragging) return;
+  };
+
+  const handleMouseUp = (event: Event) => {
+    const leftVal = (event.target as HTMLInputElement).valueAsNumber ?? 0;
+
+    const mv = App.getMusicVisualizer();
+    const d = mv.getDuration();
+    const pt = (leftVal / 100) * d;
+    duration.val = secondsToMinSec(d);
+    elapsedTime.val = secondsToMinSec(pt);
+    left.val = leftVal;
+    mv.playFromOffset(pt);
+    isDragging = false;
+  };
 
   return div(
     {
@@ -29,15 +55,17 @@ export const SeekBar = () => {
       },
       elapsedTime,
     ),
-    div(
-      {
-        class: "seek-bar-track",
-      },
-      div({
-        class: "seek-bar-thumb",
-        style: () => `left: ${left.val}%;`,
-      }),
-    ),
+    input({
+      type: "range",
+      min: "0",
+      max: "100",
+      step: "0.01",
+      value: () => left.val,
+      class: "seek-bar-input",
+      onmousedown: handleMouseDown,
+      onmousemove: handleMouseMove,
+      onmouseup: handleMouseUp,
+    }),
     div(
       {
         class: "total-time",
